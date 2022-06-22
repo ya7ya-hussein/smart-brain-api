6e17bedf436f1edf3ffd84be612cf95096b5d386
Yahya Alsabahi <yahya712865444@gmail.com>
2022-06-22 23:23:59 +0800

    first changing

--- server.js
+++ server.js
@@ -47,6 +47,8 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
